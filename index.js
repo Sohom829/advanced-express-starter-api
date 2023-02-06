d2482df3c7012ca486de9a950717926cb5a65023
Mohtasim Alam Sohom <94981761+Sohom829@users.noreply.github.com>
2023-02-06 21:36:23 +0600

    Update logger and add error handler

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
+// Import App
 const app = require("./server/app");
+// Import Configuration obj.
 const { Configuration } = require("./config.js");
+// Import chalk for logging.
 const chalk = require("chalk");
+// Import CORS
+const cors = require("cors");
+// Configuration setup
 let port = Configuration.PORT;
 let hostname = Configuration.HostName;
-let appname = Configuration.LoggerConfig.APPName;
+let appName = Configuration.LoggerConfig.APPName;
 let loggerMainColor = Configuration.LoggerConfig.LoggerMainColor;
-const loggerStyle1 = Configuration.LoggerConfig.LoggerMainStyle.style1;
-const loggerStyle2 = Configuration.LoggerConfig.LoggerMainStyle.style2;
+let loggerStyle1 = Configuration.LoggerConfig.LoggerMainStyle.style1;
+let loggerStyle2 = Configuration.LoggerConfig.LoggerMainStyle.style2;
 let loggerStyleColor = Configuration.LoggerConfig.LoggerMainStyle.styleColor;
 let loggerHostNameColor = Configuration.LoggerConfig.LoggerHostNameColor;
 let loggerPortColor = Configuration.LoggerConfig.LoggerPortColor;
 let portColor = chalk.hex(`${loggerPortColor}`);
-const hostNameColor = chalk.hex(`${loggerHostNameColor}`);
+let hostNameColor = chalk.hex(`${loggerHostNameColor}`);
 let mainColor = chalk.hex(`${loggerMainColor}`);
 let styleColor = chalk.hex(`${loggerStyleColor}`);
 
+// Use CORS
+app.use(cors());
+
+// ERROR Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+// Listen to the APP.
 app.listen(port, () => {
   console.log(
     styleColor(`${loggerStyle1}`),
-    mainColor(`${appname}`),
+    mainColor(`${appName}`),
     styleColor(`${loggerStyle2}`),
     mainColor(`: Server started on`),
     hostNameColor(`${hostname}`) + ":" + portColor(`${port}`)
